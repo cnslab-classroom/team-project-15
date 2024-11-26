241fc1caa81269e392ff56b08eb5fa928899bc48
Neouul <fluctu0q0@gmail.com>
2024-11-27 00:04:08 +0900

    contact ver 2.00 - add List, Adapter, adding number

--- app/src/main/java/com/team15/oppteamproject/MainActivity.java
+++ app/src/main/java/com/team15/oppteamproject/MainActivity.java
@@ -2,13 +2,34 @@ package com.team15.oppteamproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageView onoffBtn, contactBtn, messageBtn, settingBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        onoffBtn = findViewById(R.id.onOffBtn);
+        contactBtn = findViewById(R.id.contactBtn);
+        messageBtn = findViewById(R.id.messageBtn);
+        settingBtn = findViewById(R.id.settingBtn);
+
+        contactBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, ContactActivity.class));
+                finish();
+            }
+        });
+
+
+
     }
 }
